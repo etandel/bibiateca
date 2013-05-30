a5f654919cbc987d262dcea828a31840d6951cf3
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2013-05-30 14:25:06 -0300

    Create book and author models; activate library app.

--- bibiateca/library/models.py
+++ bibiateca/library/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author)
+    edition = models.PositiveIntegerField(null=True, blank=True)
+
