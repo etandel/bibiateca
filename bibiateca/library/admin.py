88aa62888b9831f5539ad5b35a944a8ea7049cf9
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2013-05-30 15:01:08 -0300

    Improve book list_display.

--- bibiateca/library/admin.py
+++ bibiateca/library/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from bibiateca.library.models import Author, Book
 
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'edition')
+
+
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
